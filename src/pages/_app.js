023d3eb53687d2d105a050030775f844040a678f
colbyfayock <fayock@gmail.com>
2021-06-03 00:24:43 -0400

    simplifying starter

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,17 +1,7 @@
-import { ApolloProvider } from '@apollo/client';
-
-import useApolloClient from 'hooks/use-apollo-client';
-
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps = {} }) {
-  const apolloClient = useApolloClient(pageProps.initialApolloState);
-
-  return (
-    <ApolloProvider client={apolloClient}>
-      <Component {...pageProps} />
-    </ApolloProvider>
-  );
+  return <Component {...pageProps} />;
 }
 
 export default MyApp
